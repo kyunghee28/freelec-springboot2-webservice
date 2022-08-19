e3896c0349f6e52b4829a3e8ca3bfda1fa095843
kyunghee28 <pori4339@naver.com>
2022-08-19 15:19:13 +0900

    등록 API

---
+++ src/main/java/com/kyung/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.kyung.book.springboot.service.posts;
+
+import com.kyung.book.springboot.domain.posts.PostsRepository;
+import com.kyung.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+}
--- src/test/java/com/kyung/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/kyung/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -22,7 +22,7 @@ public class HelloResponseDtoTest {
 }
 
 /**
-*  1) assertThat
+ *  1) assertThat
  *      -> assertj 라는 테스트 검증 라이브러리의 검증 메소드이다.
  *      -> 검증하고 싶은 대상을 메소드 인자로 받는다.
  *      -> 메소드 체이닝이 지원되어 isEqualTo와 같이 메소드를 이어서 사용할 수 있다.
@@ -30,5 +30,5 @@ public class HelloResponseDtoTest {
  *  2) isEqualTo
  *      -> assertj의 동등 비교 메소드
  *      -> assertThat 에 있는 값과 isEqualTo의 값을 비교해서 같을 때만 성공
-*
-* */
+ *
+ * */
